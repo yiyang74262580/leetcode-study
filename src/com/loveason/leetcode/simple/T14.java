be3fbd7c6d30628c065e8ed49ce33f2db32193d5
yiyang74262580@gmail.com <yiyang74262580@gmail.com>
2019-07-01 00:03:11 +0800

    T14题目解决

--- src/com/loveason/leetcode/simple/T14.java
+++ src/com/loveason/leetcode/simple/T14.java
@@ -7,10 +7,59 @@ package com.loveason.leetcode.simple;
  */
 public class T14 {
     public static void main(String[] args) {
-
+        String[] strs = {"aaa","aa","aaa"};
+        System.out.println(longestCommonPrefix(strs));
     }
 
     private static String longestCommonPrefix(String[] strs) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        int length = strs.length;
+        // 空数组
+        if (length == 0) {
+            return "";
+        }
+        // 数组中只有一个元素
+        if (length == 1) {
+            return strs[0];
+        }
+
+        String top = strs[0];
+
+        int min = top.length();
+        String minStr = top;
+
+        for (String str : strs) {
+            if (min > str.length()) {
+                min = str.length();
+                minStr = str;
+            }
+        }
+
+        // 字符的下标
+        int index = 0;
+        // 最终字符的长度
+        int len = 0;
+        char t = 0;
+        // 不能超过最小长度，提高效率
+        for (int i = 0; i < min; i++) {
+            for (String str : strs) {
+                // 如果长度最小的字符串的每个字符和每一个元素的每一个字符相等
+                if (minStr.charAt(index) == str.charAt(index)) {
+                    t++;
+                }
+            }
+            index++;
+            // 如果长度最小的字符串的每一个字符跟后面的字符串的每一个字符相等，
+            if (t == length * index) {
+                len++;
+            }
+        }
+
+        // 从最小长度的字符串中按下标依次取出
+        for (int i = 0; i < len; i++) {
+            result.append(minStr.charAt(i));
+        }
+
+        return result.toString();
     }
 }
