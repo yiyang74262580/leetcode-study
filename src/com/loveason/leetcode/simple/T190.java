a090df0ddd539a93336a1017df9a7e5c45d1373a
yiyang74262580@gmail.com <yiyang74262580@gmail.com>
2019-08-22 10:33:27 +0800

    T190题目修改

--- src/com/loveason/leetcode/simple/T190.java
+++ src/com/loveason/leetcode/simple/T190.java
@@ -4,14 +4,14 @@ package com.loveason.leetcode.simple;
 /**
  * @auther: alec
  * @date: 2018/8/2 10:45
- * @description:
+ * @description: 颠倒二进制位
  */
 public class T190 {
     public static void main(String[] args) {
-        System.out.println(test(1));
+        System.out.println(reverseBits(1));
     }
 
-    private static Integer test(int n) {
+    private static int reverseBits(int n) {
         int result = 0;
         for (int i = 0; i < 32; i++) {
             result += n & 1;
