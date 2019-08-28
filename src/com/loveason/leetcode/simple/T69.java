0dd7f78e7a6e7cc9a1833449b391ad806e6fe938
yiyang74262580@gmail.com <yiyang74262580@gmail.com>
2019-08-28 12:56:14 +0800

    T69题目完成

---
+++ src/com/loveason/leetcode/simple/T69.java
@@ -0,0 +1,30 @@
+package com.loveason.leetcode.simple;
+
+import java.util.Date;
+import java.util.Timer;
+
+/**
+ * @author alec
+ * @description  x 的平方根
+ * @date 11:14 2019/8/28
+ */
+public class T69 {
+    public static void main(String[] args) {
+//        int x = 4;
+        int x = 8;
+//        int x = 2147483647;
+        int sqrt = mySqrt(x);
+        System.out.println(sqrt);
+    }
+
+    private static int mySqrt(int x) {
+        // int最大值的平方根 46340
+        for (int i = 46340; i > 0; i--) {
+            int tmp = i * i;
+            if (tmp <= x) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
