da34d998e863f52b0411153fee0a2616272fce1f
yangjie <1925507543@qq.com>
2020-09-11 23:01:04 +0800

    T1512完成

---
+++ src/com/loveason/leetcode/simple/T1512.java
@@ -0,0 +1,29 @@
+package com.loveason.leetcode.simple;
+
+import java.util.HashSet;
+
+/**
+ * @author yangjie
+ * @date 2020/9/11 21:26
+ */
+public class T1512 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,1,1,3};
+//        int[] nums = new int[]{1,1,1,1};
+        System.out.println(new T1512().numIdenticalPairs(nums));
+    }
+
+    public int numIdenticalPairs(int[] nums) {
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    x++;
+                }
+            }
+        }
+
+        return x;
+    }
+}
