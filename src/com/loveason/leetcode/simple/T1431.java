35ec3c5126bd4ff138c9e00b7eaacf6525ec12a6
yangjie <1925507543@qq.com>
2020-09-13 10:24:52 +0800

    T1431完成

---
+++ src/com/loveason/leetcode/simple/T1431.java
@@ -0,0 +1,36 @@
+package com.loveason.leetcode.simple;
+
+import java.util.*;
+
+/**
+ * @author yangjie
+ * @date 2020/9/12 22:17
+ */
+public class T1431 {
+
+    public static void main(String[] args) {
+        int[] candies = new int[]{2,3,5,1,3};
+        int extraCandies = 3;
+        System.out.println(new T1431().kidsWithCandies(candies, extraCandies));
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int m = 0;
+        for (int candy : candies) {
+            if (candy > m) {
+                m = candy;
+            }
+        }
+
+        List<Boolean> list = new ArrayList<>(candies.length);
+        for (int candy : candies) {
+            if (candy + extraCandies >= m) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+
+        return list;
+    }
+}
